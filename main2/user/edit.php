e90c6b94a7d1ee10b0efbe98d6aa914a0885db4e
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-11-13 01:07:49 +0530

    Update edit.php

--- main2/user/edit.php
+++ main2/user/edit.php
@@ -1,81 +1,27 @@
 <?php
 include("config.php");
 
-$id = $_GET['id'];
-$fname = $_GET['fname'];
-$lname = $_GET['lname'];
-$pnum = $_GET['pnum'];
-$city = $_GET['city'];
-$email = $_GET['email'];
-$status = $_GET['status'];
-$age = $_GET['age'];
-$code = $_GET['code'];
+if (isset($_POST['submit'])) {
+	$id = $_POST['id'];
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$pnum = $_POST['pnum'];
+	$city = $_POST['city'];
+	$email = $_POST['email'];
+	$status = $_POST['status'];
+	$age = $_POST['age'];
+	$code = $_POST['code'];
 
-?>
+	$query = "UPDATE users SET firstName='$fname', lastName='$lname', phonenumber='$pnum', email='$email', city='$city', age='$age', code='$code', status='$status' WHERE id='$id'";
+	$data = mysqli_query($conn, $query);
 
-
-<title>Edit Details</title>
-<link rel="stylesheet" href="css/style.css">
-
-<body>
-	<div class="for-center">
-		<div class="wrapper">
-			<div class="reg-form" id="reg-form">
-				<div class="form-heading">
-					<h1>Welcome</h1>
-					<p>Let's create your account!</p>
-				</div>
-				<div class="form-inner">
-					<form action="edit.php" method="POST">
-						<input type="hidden" name="id" id="id" placeholder="First Name" value="<?php echo "$id" ?>">
-						<input type="text" name="fname" id="fname" placeholder="First Name" value="<?php echo "$fname" ?>">
-						<input type="text" name="lname" id="lname" placeholder="Last Name" value="<?php echo "$lname" ?>">
-						<input type="number" name="pnum" id="pnum" placeholder="Enter Phone Number" value="<?php echo "$pnum" ?>">
-						<select name="city" id="city" value="<?php echo "$city" ?>">
-							<option value="">Choose City</option>
-							<option value="Pakistan">Pakistan</option>
-							<option value="London">London</option>
-							<option value="Egypt">Egypt</option>
-						</select>
-						<input type="email" name="email" id="email" placeholder="Enter Your Email" value="<?php echo "$email" ?>">
-						<select name="status" id="status" value="<?php echo "$status" ?>">
-							<option value="">Choose Status</option>
-							<option value="Married">Married</option>
-							<option value="UnMarried">UnMarried</option>
-						</select>
-						<input type="number" name="age" id="age" placeholder="Enter your Age" value="<?php echo "$age" ?>">
-						<input type="number" name="code" id="code" placeholder="Enter Your Postcode" value="<?php echo "$code" ?>"><br>
-						<input type="submit" name="submit" value="Update">
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-	<?php
-	include("config.php");
-
-
-	if ($_POST['submit']) {
-		$id = $_POST['id'];
-		$fname = $_POST['fname'];
-		$lname = $_POST['lname'];
-		$pnum = $_POST['pnum'];
-		$city = $_POST['city'];
-		$email = $_POST['email'];
-		$status = $_POST['status'];
-		$age = $_POST['age'];
-		$code = $_POST['code'];
-
-		$query = "UPDATE users SET firstName='$fname', lastName='$lname', phonenumber='$pnum', email='$email', city='$city', age='$age', code='$code', status='$status' WHERE id='$id' ";
-		$data = mysqli_query($conn, $query);
-
-		if ($data) {
-			// echo "Data Updated";
-			header("Location: http://localhost/main2/user/index.php");
-		} else {
-			echo "Data not updated";
-		}
+	if ($data) {
+		header("Location: http://localhost/main2/user/index.php");
+		exit();
+	} else {
+		echo "Data not updated";
 	}
-	?>
+} else {
+	echo "Invalid request";
+}
+?>
