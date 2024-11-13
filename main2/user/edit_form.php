e6cbf6e78ca3efb3d7cc6426e0568a2357e7b69f
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-11-14 00:30:08 +0530

    Update edit_form.php

--- main2/user/edit_form.php
+++ main2/user/edit_form.php
@@ -21,24 +21,24 @@ $code = $_GET['code'];
         <div class="wrapper">
             <div class="reg-form" id="reg-form">
                 <form action="edit.php" method="POST">
-                    <input type="hidden" name="id" id="id" placeholder="First Name" value="<?php echo "$id" ?>">
-                    <input type="text" name="fname" id="fname" placeholder="First Name" value="<?php echo "$fname" ?>">
-                    <input type="text" name="lname" id="lname" placeholder="Last Name" value="<?php echo "$lname" ?>">
-                    <input type="number" name="pnum" id="pnum" placeholder="Enter Phone Number" value="<?php echo "$pnum" ?>">
-                    <select name="city" id="city" value="<?php echo "$city" ?>">
+                    <input type="hidden" name="id" id="id" placeholder="First Name" value="<?php echo $id ?>">
+                    <input type="text" name="fname" id="fname" placeholder="First Name" value="<?php echo $fname ?>">
+                    <input type="text" name="lname" id="lname" placeholder="Last Name" value="<?php echo $lname ?>">
+                    <input type="number" name="pnum" id="pnum" placeholder="Enter Phone Number" value="<?php echo $pnum ?>">
+                    <select name="city" id="city" value="<?php echo $city ?>">
                         <option value="">Choose City</option>
                         <option value="Pakistan">Pakistan</option>
                         <option value="London">London</option>
                         <option value="Egypt">Egypt</option>
                     </select>
-                    <input type="email" name="email" id="email" placeholder="Enter Your Email" value="<?php echo "$email" ?>">
-                    <select name="status" id="status" value="<?php echo "$status" ?>">
+                    <input type="email" name="email" id="email" placeholder="Enter Your Email" value="<?php echo $email ?>">
+                    <select name="status" id="status" value="<?php echo $status ?>">
                         <option value="">Choose Status</option>
                         <option value="Married">Married</option>
                         <option value="UnMarried">UnMarried</option>
                     </select>
-                    <input type="number" name="age" id="age" placeholder="Enter your Age" value="<?php echo "$age" ?>">
-                    <input type="number" name="code" id="code" placeholder="Enter Your Postcode" value="<?php echo "$code" ?>"><br>
+                    <input type="number" name="age" id="age" placeholder="Enter your Age" value="<?php echo $age ?>">
+                    <input type="number" name="code" id="code" placeholder="Enter Your Postcode" value="<?php echo $code ?>"><br>
                     <input type="submit" name="submit" value="Update">
                 </form>
             </div>
