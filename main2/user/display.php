7b896f09cf5d5ad9d9a5c28b6529e118265e76ab
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-11-14 03:47:01 +0530

    Update display.php

--- main2/user/display.php
+++ main2/user/display.php
@@ -1,8 +1,9 @@
 <style>
-	
-	td,th {
-		padding: 10px !important;
-	}
+  td,
+  th {
+    padding: 10px !important;
+  }
+
   td a.delbtn {
     color: #000;
     text-decoration: none;
@@ -10,7 +11,7 @@
     padding: 6px;
     background-color: tomato;
     border-radius: 20px;
-}
+  }
 
   td a:hover.delbtn {
     color: #fff;
@@ -37,7 +38,6 @@ $query_for_display = "SELECT * FROM users";
 $data = mysqli_query($conn, $query_for_display);
 
 $total = mysqli_num_rows($data);
-
 ?>
 <table class="table table-success table-striped">
   <thead>
@@ -53,12 +53,37 @@ $total = mysqli_num_rows($data);
       <th scope="col">Username</th>
       <th scope="col">Status</th>
       <th scope="col">Operations</th>
-	   
+
     </tr>
   </thead>
   <tbody>
     <?php
-    if ($total != 0) {
+    if (mysqli_num_rows($data) > 0) {
+      $id = 1;
+      foreach ($data as $d) {
+        echo "<tr>
+            <td>" . $id . "</td>
+            <td>" . $d['firstName'] . "</td>
+            <td>" . $d['lastName'] . "</td>
+            <td>" . $d['phonenumber'] . "</td>
+            <td>" . $d['email'] . "</td>
+            <td>" . $d['city'] . "</td>
+            <td>" . $d['age'] . "</td>
+            <td>" . $d['code'] . "</td>
+            <td>" . $d['username'] . "</td>
+            <td>" . $d['status'] . "</td>
+            <td><a class='delbtn' href='delete.php?id=$d[id]' >Delete</a>
+            <a class='editbtn' href='edit_form.php?id=$d[id]'>Edit</a>
+            </td>
+            </tr>";
+        $id++;
+      }
+    } else {
+      echo "<tr><td colspan='11'>No record found</td></tr>";
+    }
+
+
+    /* if ($total != 0) {
       $id = 1;
       while ($result = mysqli_fetch_assoc($data)) {
         echo "<tr>
@@ -73,14 +98,14 @@ $total = mysqli_num_rows($data);
             <td>" . $result['username'] . "</td>
             <td>" . $result['status'] . "</td>
             <td><a class='delbtn' href='delete.php?id=$result[id]' >Delete</a>
-            <a class='editbtn' href='edit_form.php?id=$result[id]&fname=$result[firstName]&lname=$result[lastName]&pnum=$result[phonenumber]&email=$result[email]&city=$result[city]&status=$result[status]&age=$result[age]&code=$result[code]'>Edit</a>
+            <a class='editbtn' href='edit_form.php?id=$result[id]'>Edit</a>
             </td>
             </tr>";
         $id++;
       }
     } else {
       echo "<tr><td colspan='11'>No record found</td></tr>";
-    }
+    } */
 
     ?>
   </tbody>
